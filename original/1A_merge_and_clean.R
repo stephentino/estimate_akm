ef7c81468a6ebac4cfce66b0ca964c7e7de3abd2
Stephen Tino <126087688+stephentino@users.noreply.github.com>
2023-09-11 00:28:29 -0400

    Update 1A_merge_and_clean.R

--- original/1A_merge_and_clean.R
+++ original/1A_merge_and_clean.R
@@ -1,5 +1,19 @@
-
-# Author: Stephen Tino (2023)
+# -------------------------------------------------------------------------------------------------------------------- #
+# Copyright 2023 Stephen Tino                                                                                          #
+#                                                                                                                      #
+# This file is part of estimate_akm                                                                                    #
+#                                                                                                                      #
+# estimate_akm is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General    #
+# Public License as published by the Free Software Foundation, either version 2.1 of the License, or (at your option)  #
+# any later version.                                                                                                   #
+#                                                                                                                      #
+# estimate_akm is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied   #
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more  #
+# details.                                                                                                             #
+#                                                                                                                      #
+# You should have received a copy of the GNU Lesser General Public License along with estimate_akm. If not, see        #
+# <https://www.gnu.org/licenses/>.                                                                                     #
+# -------------------------------------------------------------------------------------------------------------------- #
 
 rm(list = ls())
 
